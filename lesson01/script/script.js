5db2dfef87367fba28a78acb3e881d53f4c36cb3
Vladislav Loskutov <vla-loskuto@yandex.ru>
2020-10-12 18:54:31 +0300

    Доделан второй урок

--- lesson01/script/script.js
+++ lesson01/script/script.js
@@ -4,13 +4,16 @@ let addExpenses = 'Интернет, Такси, Коммуналка';
 let deposit = true;
 let mission = 2000000;
 let period = 12;
+let budgetDay = money / 30; //дневной доход
 
 console.log('money: ' + typeof money);
 console.log('income: ' + typeof income);
 console.log('deposit: ' + typeof deposit);
 console.log('Длина строки: ' + addExpenses.length);
 console.log(`Период равен ${period} месяцев. Цель заработать ${mission} рублей`);
-console.log('Строка в нижнем регистре: ' + addExpenses.toLowerCase());
 
+console.log('Строка в нижнем регистре: ' + addExpenses.toLowerCase());
 let arr = addExpenses.split(',');
 console.log(arr);
+
+console.log(`Дневной доход ${budgetDay}`);
