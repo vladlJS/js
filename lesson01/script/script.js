edbe63a76d56fb2d5fea0bbd3a37eaf7678e9357
Vladislav Loskutov <vla-loskuto@yandex.ru>
2020-10-12 18:26:17 +0300

    Присвоены значения переменным

--- lesson01/script/script.js
+++ lesson01/script/script.js
@@ -1,9 +1,9 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 100000;
+let income = 'фриланс';
+let addExpenses = 'интернет, такси, коммуналка';
+let deposit = true;
+let mission = 2000000;
+let period = 12;
 
 alert('Любое сообщение');
 console.log('Сообщение в консоле');
