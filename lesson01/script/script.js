1d09dc4aa9a5da49ed593cb0c71effceb7e6b2c6
Vladislav Loskutov <vla-loskuto@yandex.ru>
2020-10-12 18:42:14 +0300

    Выполнены 3 пункта из второго задания

--- lesson01/script/script.js
+++ lesson01/script/script.js
@@ -1,9 +1,12 @@
 let money = 100000;
 let income = 'фриланс';
-let addExpenses = 'интернет, такси, коммуналка';
+let addExpenses = 'Интернет, Такси, Коммуналка';
 let deposit = true;
 let mission = 2000000;
 let period = 12;
 
-alert('Любое сообщение');
-console.log('Сообщение в консоле');
+console.log('money: ' + typeof money);
+console.log('income: ' + typeof income);
+console.log('deposit: ' + typeof deposit);
+console.log('Длина строки: ' + addExpenses.length);
+console.log(`Период равен ${period} месяцев. Цель заработать ${mission} рублей`);
