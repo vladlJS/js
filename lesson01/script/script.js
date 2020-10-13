947bf95b686cbf2856a324f151b6547f46c07428
Vladislav Loskutov <vla-loskuto@yandex.ru>
2020-10-13 16:53:35 +0300

    Выполнено 8 заданий из третьего урока, добавлены новые переменные

--- lesson01/script/script.js
+++ lesson01/script/script.js
@@ -1,3 +1,5 @@
+'use strickt'
+
 let money = 100000;
 let income = 'фриланс';
 let addExpenses = 'Интернет, Такси, Коммуналка';
@@ -5,6 +7,7 @@ let deposit = true;
 let mission = 2000000;
 let period = 12;
 let budgetDay = money / 30; //дневной доход
+let expenses1, expenses2, amount1, amount2, budgetMonth;
 
 console.log('money: ', typeof money);
 console.log('income: ', typeof income);
@@ -17,3 +20,23 @@ let arr = addExpenses.split(',');
 console.log(arr);
 
 console.log(`Дневной доход ${budgetDay}`);
+
+// Третий урок
+money = prompt('Ваш месячный доход?');
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+console.log(addExpenses);
+deposit = confirm('Есть ли у вас депозит в банке?');
+console.log(deposit);
+expenses1 = prompt('Введите обязательную статью расходов');
+console.log(expenses1);
+amount1 = +prompt('Во сколько это обойдётся?');
+console.log(+amount1);
+expenses2 = prompt('Введите обязательную статью расходов');
+console.log(expenses2);
+amount2 = +prompt('Во сколько это обойдётся?');
+console.log(amount2);
+budgetMonth = money - (amount1 + amount2); //оставшийся бюджет на месяц.
+console.log(budgetMonth);
+console.log(`За ${Math.ceil(mission/budgetMonth)} месяцев будет достигнута цель ${mission} рублей/долларов/евро.`);
+budgetDay = budgetMonth / 30; //дневной бюджет
+console.log(`Дневной бюджет ${Math.floor(budgetDay)}`);
